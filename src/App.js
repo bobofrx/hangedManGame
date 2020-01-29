67ce7e69a2112884b51e376b7fcb973c8c6f4678
Boris Faroux <boris.faroux@groupeassu2000.fr>
2020-01-29 15:09:19 +0100

    Init affichage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+const PHRASES = `
+AVEC REACT LA VIE EST BELLE
+DOMMAGE ELIANE
+IL EST SYMPA CE PETIT EXO
+J’ADORE REACT !
+LA VIE L’UNIVERS ET LE RESTE
+OPENCLASSROOMS EST MON AMI
+`
+
+  .trim()
+  .split('\n')
+
+class App extends Component {
+
+  state = this.generateInitialState();
+
+  generateInitialState() {
+    const phrase = PHRASES[Math.floor(Math.random() * PHRASES.length)]
+    const usedLetters = new Set()
+    const display = this.computeDisplay(phrase, usedLetters)
+
+    return { phrase, display, usedLetters, won: false}
+  }
+
+  computeDisplay(phrase, usedLetters) {  
+    return phrase.replace(/\w/g, (letter) => (usedLetters.has(letter) ? letter : '_'))
+  }
+
+  handletter(letter){
+
+  }
+
+  reset() {
+    this.setState(this.generateInitialState()) 
+  }
+
+  render() {
+    const { phrase, usedLetters, display, won} = this.state
+
+    return (
+      <div className={`hangman ${(won && 'won') || ''}`}>
+        <p className="display">{display}</p>
+        <p className="letters">
+          {won ? (
+            <button className="replay" onClick={() => this.reset()}>
+              Rejouer
+            </button>
+          ) : (
+            ALPHABET.map((letter) => (
+              <button disabled={usedLetters.has(letter)} key={letter} onClick={() => this.handletter(letter)}>
+                {letter}
+              </button>
+            ))
+          )}
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      </div>
+    );
+  }
 }
 
 export default App;
